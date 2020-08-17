714ded6e52e0a1f26cf5c46ad802c3f0f0b987e2
nzer0 <tkmgin0914@gmail.com>
2020-08-17 16:43:20 +0900

    chore: lint vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,10 @@
-
-const DEVELOPMEANT_DIST_PATH = "../peers-server-example/web_dev";
-const PRODUCTION_DIST_PATH = "../peers-server-example/web";
+const DEVELOPMEANT_DIST_PATH =
+  "../peers-server-example-express-typescript/web_dev";
+const PRODUCTION_DIST_PATH = "../peers-server-example-express-typescript/web";
 module.exports = {
   transpileDependencies: ["vuetify"],
-  outputDir: process.env.NODE_ENV === "development" ? DEVELOPMEANT_DIST_PATH : PRODUCTION_DIST_PATH
+  outputDir:
+    process.env.NODE_ENV === "development"
+      ? DEVELOPMEANT_DIST_PATH
+      : PRODUCTION_DIST_PATH
 };
